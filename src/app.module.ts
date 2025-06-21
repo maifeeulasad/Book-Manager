3c7279e16d6b06c200a240583de586ca8a76b2ec
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-21 17:32:23 +0600

    [module]: imported all modules to root;  - author  - books  - mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
+import { MongoModule } from './db/MongoModule';
 
 @Module({
-  imports: [],
+  imports: [
+    MongoModule,
+    AuthorsModule,
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
