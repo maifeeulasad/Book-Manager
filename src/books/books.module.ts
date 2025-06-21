80c0a65a07894246c1de2d02fbde8dc0f0a7c43f
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-21 17:21:55 +0600

    [module]: book module w schema;

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { Book, BookSchema } from './BookSchema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Author, AuthorSchema } from '../authors/AuthorSchema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+    MongooseModule.forFeature([{ name: Author.name, schema: AuthorSchema }]),
+  ],
   controllers: [BooksController],
-  providers: [BooksService]
+  providers: [BooksService],
+  exports: [BooksService],
 })
 export class BooksModule {}
