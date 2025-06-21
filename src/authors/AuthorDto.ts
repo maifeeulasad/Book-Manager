40b59152f68d7411ecaf13e534a10cb3bed86125
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-21 17:08:31 +0600

    [dto]: author dto; for create and update

---
+++ src/authors/AuthorDto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsOptional, IsDateString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateAuthorDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthDate?: string;
+}
+
+export class UpdateAuthorDto extends PartialType(CreateAuthorDto) {}
