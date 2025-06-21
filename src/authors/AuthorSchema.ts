2232d2b85c52201e2acdcca6a1ef94da4eef3ca0
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-21 17:15:26 +0600

    [db,schema]: author schema;

---
+++ src/authors/AuthorSchema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Author extends Document {
+
+  @Prop({ required: true }) 
+  firstName: string;
+
+  @Prop({ required: true }) 
+  lastName: string;
+
+  @Prop() 
+  bio?: string;
+
+  @Prop() 
+  birthDate?: Date;
+  
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Book' }] })
+  books: Types.ObjectId[];
+}
+
+export const AuthorSchema = SchemaFactory.createForClass(Author);
