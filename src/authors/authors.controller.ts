59ec63959e3a58ad4a6c5ac546ae446af8aefb58
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-21 17:30:26 +0600

    [controller]: crud for author controller;

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+import { Author } from './AuthorSchema';
+import { CreateAuthorDto, UpdateAuthorDto } from './AuthorDto';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private readonly authorsService: AuthorsService) {}
+
+  @Post()
+  async create(@Body() createAuthorDto: CreateAuthorDto): Promise<Author> {
+    return this.authorsService.create(createAuthorDto);
+  }
+
+  @Get()
+  async findAll(
+    @Query('page') page = '1',
+    @Query('limit') limit = '10',
+    @Query('search') search?: string,
+  ): Promise<{ data: Author[]; count: number }> {
+    const pageNum = parseInt(page, 10);
+    const limitNum = parseInt(limit, 10);
+    return this.authorsService.findAll({ page: pageNum, limit: limitNum, search });
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Author> {
+    return this.authorsService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateAuthorDto: UpdateAuthorDto,
+  ): Promise<Author> {
+    return this.authorsService.update(id, updateAuthorDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.authorsService.remove(id);
+  }
+}
