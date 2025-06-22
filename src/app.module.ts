9538546131ecf3300d342eaff7972f16e396a2bc
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-22 12:11:01 +0600

    [refactor,cleanup,module]: removed unnecessary modules from root;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { AuthorsModule } from "./authors/authors.module";
 import { BooksModule } from "./books/books.module";
 import { MongoModule } from "./db/MongoModule";
 
 @Module({
   imports: [MongoModule, AuthorsModule, BooksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
