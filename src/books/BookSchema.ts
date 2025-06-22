78c66ec93effe26b87ef4ee3445c32984a7bb8b9
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-22 11:18:07 +0600

    [refactor]: using custom isbn validor in book schema;

--- src/books/BookSchema.ts
+++ src/books/BookSchema.ts
@@ -1,21 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { IsIsbnValidate } from './BookValidator';
 
 @Schema({ timestamps: true })
 export class Book extends Document {
 
-  @Prop({ required: true }) 
+  @Prop({ required: true })
   title: string;
 
-  @Prop({ required: true, unique: true }) 
+  @Prop({ required: true, unique: true, validate: IsIsbnValidate() })
   isbn: string;
 
-  @Prop() 
+  @Prop()
   publishedDate?: Date;
 
-  @Prop() 
+  @Prop()
   genre?: string;
-  
+
   @Prop({ type: Types.ObjectId, ref: 'Author', required: true })
   author: Types.ObjectId;
 }
